19ff0ee03b239ee789af1402176cd9c4835c2c71
Haritha Vijayakumar <harithavijayakumar06@gmail.com>
2021-01-03 14:12:57 +0530

    modi

--- CalculatorApp/calc.js
+++ CalculatorApp/calc.js
@@ -1,14 +1,14 @@
 function display(val){
-    var txtbox=document.querySelector(".displaybox")
+    var txtbox=document.querySelector("#displaybox")
     txtbox.value+=val;
 }
 
 function solve(){
-    let data=document.querySelector(".displaybox")
+    let data=document.querySelector("#displaybox")
     let result=eval(data.value)
     data.value=result
 }
 
 let clearbox=()=>{
-    document.querySelector(".displaybox").value="";
+    document.querySelector("#displaybox").value="";
 }
